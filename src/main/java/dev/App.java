91091e07506f9a1dcc95ebccd3d34d5a8a7ee829
Romain857 <romainmart.85@gmail.com>
2022-01-03 11:42:31 +0100

    tp 1

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,5 +1,5 @@
 package dev;
-
+import com.github.lalyos.jfiglet.FigletFont;
 /**
  * Hello world!
  *
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        String asciiArt = FigletFont.convertOneLine("hello");
+        System.out.println(asciiArt);
     }
 }
